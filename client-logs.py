4665ec3c8b2ce92b8f057ce99e03cef21d10390d
Alexey Formin <domen-oo@yandex.ru>
2024-06-03 17:22:11 +0300

    Added output error in python script

--- client-logs.py
+++ client-logs.py
@@ -19,26 +19,21 @@ def tail_logs(container_name, reconect_time = None):
         while True:
 
             if reconect_time is not None:
-                result = subprocess.run(['docker', 'logs', '-t', '--since', reconect_time, container_name], stdout=subprocess.PIPE)
-                yield result.stdout.decode('utf-8')
+                result = subprocess.run(['docker', 'logs', '-t', '--since', reconect_time, container_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                yield result.stderr.decode('utf-8') + result.stdout.decode('utf-8')
                 reconect_time = None
                 break
 
             process = subprocess.Popen(['docker', 'logs', '--tail', '0', '-t', '-f', container_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-            ready, _, _ = select.select([process.stdout], [], [], 300)
+            ready, _, _ = select.select([process.stdout, process.stderr], [], [], 300)
             if not ready:
                 print('Since')
-                result = subprocess.run(['docker', 'logs', '-t', '--since', '299s', container_name], stdout=subprocess.PIPE)
-                yield result.stdout.decode('utf-8')
+                result = subprocess.run(['docker', 'logs', '-t', '--since', '299s', container_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                yield result.stderr.decode('utf-8') + result.stdout.decode('utf-8')
                 break
 
-            line = process.stdout.readline()
-
-            if not line:
-                break
-
-            yield line.decode('utf-8')
+            yield process.stderr.readline().decode('utf-8') + process.stdout.readline().decode('utf-8')
 
 def main(container_name, db_name, host, port, reconect_time):
 
@@ -51,6 +46,9 @@ def main(container_name, db_name, host, port, reconect_time):
             print("Connected.")
 
             for data in tail_logs(container_name, reconect_time):
+
+                if not data: continue
+
                 obj = []
 
                 for line in data.split('\n'):
@@ -63,6 +61,7 @@ def main(container_name, db_name, host, port, reconect_time):
                     obj.append({"time": timeLog, "log": log})
 
                 if len(obj) > 0:
+                    obj = sorted(obj, key=lambda x: x['time'])
                     sock.sendall(bytes(json.dumps({"type": "add", "data": obj}) + "\n", encoding="utf-8"))
                     _reconect_time = obj[0]["time"]
 
@@ -84,7 +83,7 @@ def main(container_name, db_name, host, port, reconect_time):
     return main(container_name, db_name, host, port, _reconect_time)
 
 if __name__ == "__main__":
-    container_name = "streamlive-3413"
+    container_name = "streamlive-kick-32307"
     db_name = "Test"
     host = "127.0.0.1"
     port = 34092
